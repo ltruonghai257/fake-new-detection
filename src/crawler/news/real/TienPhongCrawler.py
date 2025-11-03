b8da3da084d1aa070c8febb8684ecd6c123154ec
latruonghai <latruonghai2507@icloud.com>
2025-11-03 21:45:45 +0700

    feat(fake_news): :zap: integrate crawler with datasets

---
+++ src/crawler/news/real/TienPhongCrawler.py
@@ -0,0 +1,35 @@
+from crawler.base_crawler import BaseCrawler
+from crawler.typings import SelectorType
+
+
+class TienPhongCrawler(BaseCrawler):
+    def __init__(self) -> None:
+        super().__init__(url="https://tienphong.vn/")
+
+    @property
+    def title_selector(self) -> SelectorType:
+        # Placeholder: Please inspect the website and update this selector
+        return {"css_selector": ["h1.article__title"]}
+
+    @property
+    def image_selector(self) -> SelectorType:
+        # Placeholder: Please inspect the website and update this selector
+        return {
+            "css_selector": ["figure.article__figure"],
+            "image_tag_selector": "img",
+            "caption_tag_selector": "figcaption",
+            "image_tag_attr": "src",
+        }
+
+    @property
+    def link_selector(self) -> SelectorType:
+        return {"css_selector": ["a"]}
+
+    @property
+    def content_selector(self) -> SelectorType:
+        # Placeholder: Please inspect the website and update this selector
+        return {"css_selector": ["div.article__body > p"]}
+
+    @property
+    def url_prefix(self) -> str:
+        return "https://tienphong.vn"
